4fe0e430d060f229ac71a4120fb7c2aaba9014c9
haziman bin hashim <haziman@abh.my>
2017-01-01 00:44:40 +0800

    update requirement checking comparison bug

--- checkreqs.bat
+++ checkreqs.bat
@@ -5,13 +5,13 @@ FOR /f %%a IN ('wmic ComputerSystem get TotalPhysicalMemory ^|findstr /V /n "Tot
 )
 SET pmem=%pmem:~2%
 SET rmem=4294967296
-set "tmp_pmem=               %num1%"
-set "tmp_rmem=               %num2%"
+set "tmp_pmem=               %pmem%"
+set "tmp_rmem=               %rmem%"
 set pad1="%tmp_pmem:~-15%"
 set pad2="%tmp_rmem:~-15%"
 
 ECHO|SET /P=Physical Memory: %pmem%
-IF NOT [%pad1%] GTR [%pad2%] (
+IF NOT %pad1% GTR %pad2% (
 	ECHO [FAIL]
 	ECHO Your physical memory is not enough. Have you been eating ants lately? 
 	ECHO Just kidding. You should upgrade your RAM to at least 4GB.
